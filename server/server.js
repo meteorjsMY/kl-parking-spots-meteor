15272beac8f1afeff5c82cc86dc478b357bd0c43
Firdaus Ramlan <firdaus.ramlan@gmail.com>
2015-06-26 03:01:10 +0800

    call api directly

--- server/server.js
+++ server/server.js
@@ -1,3 +1,38 @@
+var klParkingSpots = Meteor.npmRequire('kl-parking-spots');
+
+var updateSpots = function(){
+
+  klParkingSpots(Meteor.bindEnvironment(function(spots){
+
+    if(spots){
+
+      var map = {
+        id: '_id'
+      }
+
+      _.each(spots, function(spot){
+
+        spot = _.reduce(spot, function(result, value, key) {
+          key = map[key] || key;
+          result[key] = value;
+          return result;
+        }, {});
+
+        ParkingSpot.upsert(
+          {
+            _id: spot._id
+          },
+          spot
+        );
+
+      })
+
+    }
+
+  }));
+
+}
+
 Meteor.startup(function(){
 
   SyncedCron.add({
@@ -5,43 +40,10 @@ Meteor.startup(function(){
     schedule: function(parser) {
       return parser.text('every 30 seconds');
     },
-    job: function() {
-
-      HTTP.get('https://kl-parking-spots.herokuapp.com/', function(error, results){
-
-        if(!error){
-
-          var map = {
-            id: '_id'
-          }
-
-          _.each(results.data,  function(spot){
-
-            spot = _.reduce(spot, function(result, value, key) {
-              key = map[key] || key;
-              result[key] = value;
-              return result;
-            }, {});
-
-            ParkingSpot.upsert(
-              {
-                _id: spot._id
-              },
-              spot
-            );
-
-          })
-
-        }
-        else {
-          console.log(error)
-        }
-
-      })
-
-    }
+    job: updateSpots
   });
 
   SyncedCron.start();
+
 
 })
